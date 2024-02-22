c7402ae70fc45852aaaa851c6139ff3dea8838d3
Digital Creativity <digitalcreativity@Digitals-iMac.home>
2024-02-22 11:07:36 +0200

    handle middle btn position

--- Sources/CardTabBarView.swift
+++ Sources/CardTabBarView.swift
@@ -95,6 +95,19 @@ class CardTabBarView: UITabBar {
         }
     }
     
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView?
+      {
+        guard !clipsToBounds && !isHidden && alpha > 0 else { return nil }
+        for member in subviews.reversed()
+        {
+            let subPoint = member.convert(point, from: self)
+            guard let result = member.hitTest(subPoint, with: event)
+            else { continue }
+            return result
+        }
+        return nil
+      }
+    
     private func setupConstraint() {
         addSubview(backgroundShadowView)
         addSubview(container)
